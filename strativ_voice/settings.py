804102d89a80c74740d485c575c656f1dc09b6aa
Sudipto Das <sudipto@strativ.se>
2026-04-10 17:50:44 +0600

    Added jazzmin

--- strativ_voice/settings.py
+++ strativ_voice/settings.py
@@ -28,6 +28,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(','
 
 # Application definition
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
