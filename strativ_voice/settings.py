26a52e59fede3548143bcc105058883c5fcff394
Sudipto Das <sudipto@strativ.se>
2026-04-10 17:56:03 +0600

    fixes

--- strativ_voice/settings.py
+++ strativ_voice/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = []
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files (user uploads)
 MEDIA_URL = '/media/'
